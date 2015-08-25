22394d8e09c9c2ddc485420289cefc6e536e3984
Martin Hofmann <martin@martinhofmann.com>
2015-08-25 15:18:34 -0400

    Instantiates gh-pages branch.

---
+++ assets/js/scripts.js
@@ -0,0 +1,17 @@
+/*!
+ * motorcycons
+ * Icons for the Powersports industry
+ * https://github.com/Martskin/motorcycons
+ * @author Martin Hofmann
+ * @version 1.0.0
+ * Copyright 2015. OFL-1.1 licensed.
+ */
+(function ($, window, document, undefined) {
+
+  'use strict';
+
+  $(function () {
+    $('body').addClass('is-loaded');
+  });
+
+})(jQuery, window, document);
